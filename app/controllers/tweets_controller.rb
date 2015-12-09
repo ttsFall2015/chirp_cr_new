e5ec1c044d63d9fb75644997f50d8c578253ca9d
Your Actual Name <Your Actual Email>
2015-12-08 19:37:04 -0500

    newest updates from shane

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,7 @@ class TweetsController < ApplicationController
   end
 
   def edit
+
   end
 
   def new
@@ -30,7 +31,23 @@ class TweetsController < ApplicationController
       end
     end
   end
+    def update
+      respond_to do |format|
+        if @tweet.update(tweet_params)
+          format.html {redirect_to @tweet, notice: 'Tweet was updated!'}
+        else
+          format.html {render :edit}
+        end
 
+      end
+    end
+
+  def destroy
+    @tweet.destroy
+    respond_to do |format|
+      format.html {redirect_to tweets_url, notice: 'Tweet was deleted for you.'}
+    end
+  end
 
   private
 
@@ -38,10 +55,11 @@ class TweetsController < ApplicationController
       @tweet = Tweet.find(params[:id])
     end
 
+
     def tweet_params
       # It's mandatory to specify the nested attributes that should be whitelisted.
       # If you use `permit` with just the key that points to the nested attributes hash,
       # it will return an empty hash.
       params.require(:tweet).permit(:message, :user_id)
     end
-end
+  end
