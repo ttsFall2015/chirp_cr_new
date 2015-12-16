3321aa48c7bc74b4e13ec7edff618744eac62f30
Renee <reneeymatthews@gmail.com>
2015-12-16 18:59:40 -0500

    updates gemfile.  REMEMBER TO RUN BUNDLE INSTALL on the local machine

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   root "tweets#index"
 
+  
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
